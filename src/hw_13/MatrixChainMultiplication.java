8e7e47af1bc05bb671cc04ee9a7b430c8862c66b
SoyeongChoi <201602084@cs-cnu.org>
2018-12-05 16:00:17 +0900

    우선 행렬 크기 같은애들끼리 곱합

--- src/hw_13/MatrixChainMultiplication.java
+++ src/hw_13/MatrixChainMultiplication.java
@@ -1,20 +1,40 @@
 package hw_13;
 
 import java.io.IOException;
+import java.util.*;
 
 public class MatrixChainMultiplication {
-	int MAX = Integer.MAX_VALUE;
+	static int MAX = Integer.MAX_VALUE;
 	public static void main(String[] args) throws IOException {
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("입력행렬의 크기를 쓰세요.(0 이상)");
+		int x = input.nextInt();
+		if(x<=0){
+			System.out.println("프로그램을 종료합니다.");
+			System.exit(0);
+		}
+		int p[] = new int[x];
+		for(int i = 0; i < x; i++){
+			System.out.println(i+"번째 행렬값을 입력하세요 ");
+			int temp = input.nextInt();
+			p[i] = temp;
+		}
+		
+		int m[][] = new int[x][x];
+		int s[][] = new int[x][x];
+		
+		MatrixChainOrder(p,m,s);
+		
 	}
 
-	public void MatrixChainOrder(int p[],int m[][],int s[][]){
+	public static void MatrixChainOrder(int p[],int m[][],int s[][]){
 		int n = p.length -1;
 		for(int i = 0; i < n; i++){
 			m[i][i] = 0;
 		}
-		for(int l = 1; l < n; l++){
-			for(int i = 0; i < n-l+1; i++){
+		for(int l = 2; l <= n; l++){
+			for(int i = 1; i <= n-l+1; i++){
 				int j = i + l -1;
 				m[i][j] = MAX;
 				for(int k = i; i < k-1; k++){
@@ -25,6 +45,21 @@ public class MatrixChainMultiplication {
 					}
 				}
 			}
+		}
+		for(int i = m.length-1; i>0; i--){
+			for(int j = 1; j < m.length; j++){
+				printOptimalParens(m, i, j);
+			}
+		}
+	}
+	
+	public static void printOptimalParens(int[][]s, int i, int j){
+		if(i == j){
+			System.out.print("A"+i);
+		}else{
+			System.out.print("(");
+			printOptimalParens(s,s[i][j]+1,j);
+			System.out.print(")");
 		}
 	}
 }
